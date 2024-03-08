6d85ccc15556aa4f5677d7a800ab19bdecd23d75
Bang Tran <bangdt3@gmail.com>
2024-03-08 16:59:28 +0700

    update

--- array-hashing/easy/contains-duplicate.java
+++ array-hashing/easy/contains-duplicate.java
@@ -1,3 +1,13 @@
-public class sdsd {
-    
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Boolean> numbers = new HashMap<>();
+        for (int n : nums) {
+            if (numbers.containsKey(n))
+                return true;
+            numbers.put(n, true);
+        }
+        return false;
+    }
 }
